3c790c9309ce7c95632f860da2cff32c430cc971
Yogesh <121231978+Logesh9384@users.noreply.github.com>
2023-01-08 02:55:44 -0800

    Add files via upload

---
+++ HomeEb.java
@@ -0,0 +1,14 @@
+class HomeEb
+{
+public static void main(String[] args)
+{
+Home home= new Home();
+home.read_eb();
+}
+void read_eb()
+{
+int units=200;
+int amount=units*2;
+System.out.println("You have To Pay"+amount);
+}
+}
