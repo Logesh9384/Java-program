8092708338818d75bca6f054606dc9c1ed1e3077
Logesh9384 <121231978+Logesh9384@users.noreply.github.com>
2023-01-08 02:43:20 -0800

    First program

---
+++ Calculator.java
@@ -0,0 +1,28 @@
+class Calculator
+{
+public static void main(String[] args)
+{
+Calculator calc= new Calculator();
+calc.add();
+calc.Subraction();
+calc.Multiply();
+calc.Divide();
+
+}
+void add()
+{
+System.out.println(10+30);
+}
+void Subraction()
+{
+System.out.println(100-40);
+}
+void Multiply()
+{
+System.out.println(10*10);
+}
+void Divide()
+{
+System.out.println(2/50);
+}
+}
